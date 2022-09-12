6a52260dc2e6bf75d6a0a44552c105461851e737
rizemon <tan.jia.le.98@gmail.com>
2022-09-12 10:18:44 +0800

    Convert credentials to Github Secrets

--- tracer_backend/settings.py
+++ tracer_backend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 
 # Vault client connection and initialization settings.
@@ -31,7 +31,7 @@ VAULT_SETTINGS = {
 }
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = (os.environ.get('DJANGO_DEBUG') == "True")
 
 ALLOWED_HOSTS = []
 
