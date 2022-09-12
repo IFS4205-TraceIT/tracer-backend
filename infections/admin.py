2ad99ce8071eee86e1f5c818f8184fe15bda62c4
Styz <sky.t4y@gmail.com>
2022-09-12 15:56:34 +0800

    Remove admin models

--- infections/admin.py
+++ infections/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 
-from .models import (
-    Users,
-    Closecontacts,
-    Infectionhistory,
-    Notifications,
-    Contacttracers
-)
-# Register your models here.
-@admin.register(Users,Closecontacts,Infectionhistory,Notifications,Contacttracers)
-class InfectionAdmin(admin.ModelAdmin):
-    pass
+# from .models import (
+#     Users,
+#     Closecontacts,
+#     Infectionhistory,
+#     Notifications,
+#     Contacttracers
+# )
+# # Register your models here.
+# @admin.register(Users,Closecontacts,Infectionhistory,Notifications,Contacttracers)
+# class InfectionAdmin(admin.ModelAdmin):
+#     pass
