a138e33c51c4a41d3af49f2d973d48a86d25f5b2
Kelvin Neo <2332196+kelvneo@users.noreply.github.com>
2022-10-22 16:54:26 +0800

    Comment out admin page

--- buildings/admin.py
+++ buildings/admin.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
+# from django.contrib import admin
 
-from .models import (
-    Buildingaccess,
-    Buildings
-)
-# Register your models here.
-@admin.register(Buildingaccess, Buildings)
-class BuildingAdmin(admin.ModelAdmin):
-    pass
+# from .models import (
+#     Buildingaccess,
+#     Buildings
+# )
+# # Register your models here.
+# @admin.register(Buildingaccess, Buildings)
+# class BuildingAdmin(admin.ModelAdmin):
+#     pass
