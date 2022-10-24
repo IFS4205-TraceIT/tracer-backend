d2553ee5cb527fa71790f56d476b5e68acf498ca
3m0W33D <sky.t4y@gmail.com>
2022-10-24 10:09:36 +0000

    Remove default date creation from serializers and add it to API call

--- infections/serializers.py
+++ infections/serializers.py
@@ -4,9 +4,8 @@ from .models import (
     Infectionhistory,
     Notifications
     )
-from django.utils import timezone
+
 from rest_framework import exceptions, serializers 
-from datetime import date, timedelta
 
 class NotificationsSerializer(serializers.ModelSerializer):
 
@@ -57,8 +56,6 @@ class CloseContactsSerializer (serializers.ModelSerializer):
         )
 
 class UpdateUploadSerializer(serializers.ModelSerializer):
-    due_date = serializers.DateField(default=timezone.now().date()+timedelta(days=7))
-    start_date = serializers.DateField(default=timezone.now().date())
 
     class Meta:
         model = Notifications
