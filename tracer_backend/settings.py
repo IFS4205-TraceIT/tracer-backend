573ddf42f7fb0bd6024753ce37ff470cb8e77f5b
3m0W33D <sky.t4y@gmail.com>
2022-10-27 07:17:50 +0000

    Include rest_framework

--- tracer_backend/settings.py
+++ tracer_backend/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     'accounts.apps.AccountsConfig',
     'infections.apps.InfectionsConfig',
